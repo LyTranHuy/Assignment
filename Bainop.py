babbbd02401e318784bc5d8721be6c1729969f40
LyTranHuy <115777706+LyTranHuy@users.noreply.github.com>
2022-10-14 22:13:29 +0700

    Add files via upload

--- Bainop.py
+++ Bainop.py
@@ -27,7 +27,7 @@ for i in range (0, len(nums)-1):
 
 #Bai 3
 
-n_str = int(input())
+"""n_str = int(input())
 wordlist = list()
 count ={}
 for i in range(n_str):
@@ -41,4 +41,55 @@ print(len(count))
 result ={}
 for i in count:
     y = count.get(i)
-    print(y, end = ' ')
+    print(y, end = ' ')"""
+
+#Bai 4
+def CheckFirstNum(s):
+    nstr = list(str(s))
+    first = int(nstr[0])
+    if first in range (4, 6):
+        return True
+    else:
+        return False
+def CheckNumber(s):
+    nstr = list(str(s))
+    number = str([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    dem = 0
+    for i in nstr:
+        if i =='-':
+            continue
+        if i not in number:
+            dem += 1
+    if dem >= 1:
+        return False
+    else:
+        return True 
+def CheckRepeated(s):
+    n = len(s)
+    ch = s[0]
+    count = 1
+    for i in range(1, n):
+        if s[i] == '-':
+            continue
+        if s[i] == ch:
+            count += 1
+            if count == 4:
+                return False
+        else:
+            ch = s[i]
+            count = 1
+    return True 
+def CheckHyphen(s):
+    if s.count('-') == 0 or s.count('-') == 3:
+        return True
+    else:
+        return False
+
+s = input()
+x = len(s)
+if x == 16 or x == 19:
+    if CheckFirstNum(s) and CheckHyphen(s) and CheckNumber(s) and CheckRepeated(s):
+        print ("Valid")
+    else:
+        print("Invalid")
+
